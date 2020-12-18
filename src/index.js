0f7e9eb6664cdf3aa229be23fed64a710b88d7ea
unknown <bocchi.dev@gmail.com>
2020-12-18 18:28:40 -0300

    states configured

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { lat:null, errorMessage:'' };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat:position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errorMessage:err.message})
+            }
+        );
+    }
+
+    render(){
+        if (this.state.lat && !this.state.errorMessage){
+            return <div>Latitude: {this.state.lat}</div>
+        }else if (!this.state.lat && this.state.errorMessage){
+            return <div>Error: {this.state.errorMessage}</div>
+        }else{
+            return <div>Loading</div>
+        }
+        
+        }
+} 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
